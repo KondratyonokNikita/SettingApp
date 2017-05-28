eee9cef478a4c9f2da6908f96f0391e8f285d9e8
KondratyonokNikita <nkondr2006@rambler.ru>
2017-05-28 20:41:26 +0300

    Сделал Reflection в Switcher

--- src/loaders/Switcher.java
+++ src/loaders/Switcher.java
@@ -1,19 +1,32 @@
 package loaders;
 
+import internal.MainApp;
 import internal.model.Setting;
-import person.Person;
 
 /**
  * Created by Samsung on 23.05.2017.
  */
 public class Switcher {
     public static Setting getObject(String type) {
-        Setting setting = null;
-        switch (type) {
-            case "person":
-                setting = new Person();
-                break;
+        try {
+            Class aClass = Class.forName(MainApp.properties.getProperty(type));
+            System.out.println(aClass.getName());
+            Setting setting = (Setting) aClass.newInstance();
+            return setting;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
         }
-        return setting;
+        return null;
+//        Setting setting = null;
+//        switch (type) {
+//            case "person":
+//                setting = new Person();
+//                break;
+//        }
+//        return setting;
     }
 }
